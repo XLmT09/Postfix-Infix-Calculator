1f58abc1de5bc55dc8fcac66ec7786788b478e76
Hijithan <zjac082@live.rhul.ac.uk>
2022-11-07 18:28:21 +0000

    Adding my setup code for javaFx.
    I have updated the pom file to allow javafx to work, by downloading the javafx dependencies.
    I have also created a Driver.java file as a staring point when the
    application runs.
    
    Feature Branch description: This branch will work mainly on building the GUI veiw of the application, so mostly only work on high level logic not low level.

---
+++ calculator/src/main/java/calculator/Driver.java
@@ -0,0 +1,28 @@
+package calculator;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * This class will be the entry point for the programme.
+ *
+ * @author HIJI
+ *
+ */
+public class Driver extends Application {
+  /**
+   * Programme will start by running main.
+   *
+   * @param args The array of command line arguments.
+   */
+  public static void main(String[] args) {
+    launch(args);
+  }
+
+  @Override
+  public void start(Stage primaryStage) throws Exception {
+    // TODO Auto-generated method stub
+    
+  }
+
+}
