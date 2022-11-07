8cf31356f100d14aee2bc4278a41a114a35a0be4
Hijithan <zjac082@live.rhul.ac.uk>
2022-11-07 23:54:59 +0000

    Created a empty GUI window.
    I created a GuiVeiw class to define my javaFX pieces.
    Ive created GUIVeiw.fxml to allow for a more easy and abstract program
    design.
    Ive modified Driver class so that it refrences GUIView.fxml.

--- calculator/src/main/java/calculator/Driver.java
+++ calculator/src/main/java/calculator/Driver.java
@@ -1,28 +1,22 @@
 package calculator;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-/**
- * This class will be the entry point for the programme.
- *
- * @author HIJI
- *
- */
-public class Driver extends Application {
-  /**
-   * Programme will start by running main.
-   *
-   * @param args The array of command line arguments.
-   */
+public class Driver extends Application{
+
   public static void main(String[] args) {
     launch(args);
   }
 
   @Override
   public void start(Stage primaryStage) throws Exception {
-    // TODO Auto-generated method stub
-    
+    Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("GUIView.fxml"));
+    Scene scene = new Scene(root, 800, 500);
+    primaryStage.setScene(scene);
+    primaryStage.show();      
   }
-
 }
