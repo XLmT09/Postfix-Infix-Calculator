cd05d6759dfbecd05109238130ae61ec4329c330
Hijithan <zjac082@live.rhul.ac.uk>
2022-11-15 18:40:38 +0000

    Creating a NumStack for Reverse Polish.
    I have created two files, one called NumStack.java and another class
    called TestNumStack.java. I have made a constructor in NumStack class
    and done a TDD test for it.
    
    Feature Branch Description:
    In this branch i will focus on creating the NumStack class, where stack
    will contain only numeric values. I am making this feature as this class
    will be needed for the reverse polish calculator.

---
+++ calculator/src/main/java/calculator/NumStack.java
@@ -0,0 +1,11 @@
+package calculator;
+
+/**
+ * The {@code NumStack} class represents a Stack of only numeric values. This is 
+ * @author HIJI
+ *
+ */
+public class NumStack {
+  
+  public NumStack() {}
+}
---
+++ calculator/src/test/java/calculator/TestNumStack.java
@@ -0,0 +1,14 @@
+package calculator;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class TestNumStack {
+  private NumStack numStack;
+  
+  @BeforeEach
+  void setUp() {
+    numStack = new NumStack();
+  }
+
+}
