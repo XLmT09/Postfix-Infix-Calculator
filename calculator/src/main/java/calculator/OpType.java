4b0a97214e3bf813f241ac3afcb7cb9ce18f9cc5
Hijithan <zjac082@live.rhul.ac.uk>
2022-11-19 00:25:57 +0000

    Create Enum Optype Class
    Commit Description:
    I create a enum class called OpType where it has two values, to
    represent how expressions should be evaluated.
    
    Branch Description:
    This feature branch is aimed to try and connect the gui and ssembly
    together so when the user types in an expression they will get the
    correct output in the GUI.

---
+++ calculator/src/main/java/calculator/OpType.java
@@ -0,0 +1,17 @@
+package calculator;
+
+/**
+ * The ways a calculator can perform evaluation on a expression.
+ *
+ * @author HIJI
+ */
+public enum OpType {
+  /**
+   * Reverse polish calculator.
+   */
+  INFIX, 
+  /**
+   * Standard calculator.
+   */
+  POSTFIX
+}
