f79414a43c204a3df2b67ce4e6b4299f9a9932a7
Hijithan <zjac082@live.rhul.ac.uk>
2022-11-21 00:39:31 +0000

    Giving GUIView backend functionality
    Commit Description:
    I first create a new class called Observer, this class will be used for
    the Contoller class i will develop later. The observer class just makes
    that when GUIView sends an update of a new expression the Contoller eill
    be notified.
    
    In the GUIView class i first define some of the componets in the screen,
    then reference the answer component so that that the setAnswer method
    can update its value.
    I also define the input component so that the getExpression method can
    retrive the user input.
    I also define a submit component, so that the addCalcObserver method can
    notify the observer when user clicks submit.

--- calculator/src/main/java/calculator/GuiView.java
+++ calculator/src/main/java/calculator/GuiView.java
@@ -1,5 +1,70 @@
 package calculator;
 
-public class GuiView {
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
 
+/**
+ * This is the JavaFX View that is filled by the FXML loader at runtime.
+ *
+ * @author HIJI, Dave
+ */
+public class GuiView implements ViewInterface{
+
+  /**
+   * This allows a new observer to be added to the change calculate strategy action.
+   *
+   * @param observer The Observer to be notified.
+   */
+  public void addCalcObserver(Observer observer) {
+    submit.setOnAction(new EventHandler<ActionEvent>() {
+
+      @Override
+      public void handle(ActionEvent event) {
+        observer.tell();
+      }
+    });
+  }
+
+  /**
+   * A hook to expose the question to another class.
+   *
+   * @return the arithmetic expression to be evaluated.
+   */
+  public String getExpression() {
+    return input.getText();
+  }
+
+  /**
+   * A hook to allow the answer to be displayed.
+   *
+   * @param result the evaluated answer.
+   */
+  public void setAnswer(String result) {
+    answer.setText(result);
+  }
+  
+  /**
+   * The text object which displays the answer on the screen.
+   */
+  @FXML
+  //fx:id="answer"
+  private Text answer; // Value injected by FXMLLoader
+
+  /**
+   * The field which allows users to input expression in the application.
+   */
+  //fx:id="input"
+  @FXML
+  private TextField input; // Value injected by FXMLLoader
+
+  /**
+   * The submit button on the application.
+   */
+  //fx:id="submit"
+  @FXML
+  private Button submit; // Value injected by FXMLLoader
 }
