47948cd4d629721151d14cfc9aa319dab3b23d3a
Hijithan <zjac082@live.rhul.ac.uk>
2022-11-29 23:16:05 +0000

    Removing Scientific Notation
    Commit Description/Usability Testing:
    After writing expressions with large numbers i noticed that outputs were
    given in E notation, this is not ideal for my calculator. As i belive
    the client wants to actually see evvery digit instead.
    I achive this by using a built in decimal format class.

--- calculator/src/main/java/calculator/controller/CalcController.java
+++ calculator/src/main/java/calculator/controller/CalcController.java
@@ -3,31 +3,38 @@ package calculator.controller;
 import calculator.InvalidExpressionException;
 import calculator.RevPolishCalc;
 import calculator.ViewInterface;
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.util.Locale;
+
 
 /**
- * The observer which will observe the {@code View}, it will
- * update its values are being notified from the view.
+ * The observer which will observe the {@code View}, it will update its values are being notified
+ * from the view.
  *
  * @author HIJI, Dave
  */
 public class CalcController {
-  //Only RevPolishPolish model made so just hard code this model for now
+  //I use decimal format to get rid of scientific notation
+  DecimalFormat df = new DecimalFormat("0", DecimalFormatSymbols.getInstance(Locale.ENGLISH));
+  // Only RevPolishPolish model made so just hard code this model for now
   private RevPolishCalc model = new RevPolishCalc();
-  //The view the controller will listen to
+  // The view the controller will listen to
   private ViewInterface view;
-  
+
   public CalcController() {}
-  
-  //Performs evaluation for a given expression, where evaluation depends on the model type
+
+  // Performs evaluation for a given expression, where evaluation depends on the model type
   private void calculate() {
     try {
-      view.setAnswer("" + model.evaluate(view.getExpression()));
+      df.setMaximumFractionDigits(340);
+      view.setAnswer("" + df.format(model.evaluate(view.getExpression())));
     } catch (InvalidExpressionException e) {
-      //custom error messages have been written, so we just call the getMessage method
+      // custom error messages have been written, so we just call the getMessage method
       view.setAnswer(e.getMessage());
     }
   }
-  
+
   /**
    * The method which the view uses to add itself to the controller.
    *
