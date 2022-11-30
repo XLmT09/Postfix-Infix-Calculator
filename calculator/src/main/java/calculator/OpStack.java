75784939fb3baa20001b6470465fe7f34ac4c039
Hijithan <zjac082@live.rhul.ac.uk>
2022-11-30 19:52:23 +0000

    Created OpStack for Shunting Algorithm
    Commit Description:
    I have created two setup files one called OpStack.java and another
    called TestOpStack.java.
    
    Feature Branch Description:
    In this branch i will create a stack which only contains operators, so
    it can be used for the shunting algorithm later on.

---
+++ calculator/src/main/java/calculator/OpStack.java
@@ -0,0 +1,12 @@
+package calculator;
+
+/**
+ * The {@code OpStack} class will be a {@link calculator.Stack Stack} of {@link calculator.Symbol
+ * Symbols} for the Shunting Algorithm. We provide a facade to hide details of the
+ * {@link calculator.Stack Stack}.
+ *
+ * @author HIJI
+ */
+public class OpStack {
+
+}
---
+++ calculator/src/test/java/calculator/TestOpStack.java
@@ -0,0 +1,17 @@
+package calculator;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class TestOpStack {
+
+  @BeforeEach
+  void setUp() throws Exception {}
+
+  @Test
+  void test() {
+    fail("Not yet implemented");
+  }
+
+}
