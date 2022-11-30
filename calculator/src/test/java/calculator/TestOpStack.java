9e2a12c3ba795aae65a48c31a272f0686713bf12
Hijithan <zjac082@live.rhul.ac.uk>
2022-11-30 21:17:22 +0000

    Pop method throw exception on empty stack
    Commit Description\TDD:
    I write a test case to see if opStack throws an exception if it tries to
    pop an empty stack.
    This test passes.
    
    Extra:
    I write an extra TDD  test to see if opStack works as intended after
    pushing two values.
    This test passes.

--- calculator/src/test/java/calculator/TestOpStack.java
+++ calculator/src/test/java/calculator/TestOpStack.java
@@ -1,7 +1,8 @@
 package calculator;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import java.util.EmptyStackException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -31,6 +32,28 @@ class TestOpStack {
     assertEquals(opStack.pop(), Symbol.MINUS,
         "Pushed a minus symbol, so when popped i should get minus symbol");
     assertEquals(opStack.isEmpty(), true, "After stack has been popped it should be empty.");
+  }
+  
+  @Test
+  void testOpStackPopException() {
+    assertThrows(EmptyStackException.class, () -> opStack.pop(), 
+        "Cannot pop from an empty stack.");
+  }
+  
+  @Test
+  void testOpStackPushAndPopTwice() {
+    opStack.push(Symbol.PLUS);
+    opStack.push(Symbol.DIVIDE);
+    assertEquals(opStack.isEmpty(), false,
+        "Adding Entry to stack wont make it empty.");
+    assertEquals(opStack.pop(), Symbol.DIVIDE,
+        "Pushed Symbol.DIVIDE last, so when popped i should get Symbol.DIVIDE");
+    assertEquals(opStack.isEmpty(), false,
+        "Still one Entry in stack so not empty.");
+    assertEquals(opStack.pop(), Symbol.PLUS,
+        "Last Entry is Symbol.PLUS, so when popped i should get Symbol.PLUS");
+    assertEquals(opStack.isEmpty(), true, 
+        "After stack has been popped it should be empty.");
   }
 
 
