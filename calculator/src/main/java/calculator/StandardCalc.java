2e946f7fb79ff023ff3b406aac34b95fe121ebc0
Hijithan <zjac082@live.rhul.ac.uk>
2022-12-01 00:00:20 +0000

    Create Constructor for Standard Calc
    Commit Description/TDD:
    I write a test in a class i created called TestStandardClac to see if a
    constructor for StandardCalc class can be made.
    The test fails, to fix this i just create a StandardCalc class and adda
    constructor to it.
    
    Feature Branch Description:
    In this branch the aim is to get evaluations for infix working in the
    assembly so that it can be connected to the GUI afterwards.

---
+++ calculator/src/main/java/calculator/StandardCalc.java
@@ -0,0 +1,11 @@
+package calculator;
+
+/**
+ * This class evaluates infix {@code String} expressions.
+ *
+ * @author HIJI
+ */
+public class StandardCalc {
+  
+  public StandardCalc() {}
+}
---
+++ calculator/src/test/java/calculator/TestStandardClac.java
@@ -0,0 +1,14 @@
+package calculator;
+
+import org.junit.jupiter.api.BeforeEach;
+
+class TestStandardClac {
+  private StandardCalc calc;
+  
+  @BeforeEach
+  void setUp() {
+    calc = new StandardCalc();
+  }
+
+
+}
