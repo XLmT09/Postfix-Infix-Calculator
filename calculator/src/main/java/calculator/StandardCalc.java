5d876abbc9c03e79654891a33ea47996e19adb48
Hijithan <zjac082@live.rhul.ac.uk>
2022-12-02 21:12:09 +0000

    Add javadoc for Standard Calc
    Commit Description:
    I add javadoc and comments for the StandardCalc class.

--- calculator/src/main/java/calculator/StandardCalc.java
+++ calculator/src/main/java/calculator/StandardCalc.java
@@ -9,11 +9,16 @@ public class StandardCalc implements Calculator {
   private OpStack opStack;
   private RevPolishCalc rpCalc;
 
+  /**
+   * Initialises an empty {@link calculator.OpStack OpStack} and creates a
+   * {@link calculator.RevPolishClac RevPolishCalc} object.
+   */
   public StandardCalc() {
     opStack = new OpStack();
     rpCalc = new RevPolishCalc();
   }
 
+  // Check which operator has the greatest priority.
   private int precedence(String operation) {
     switch (operation) {
       case "/":
@@ -51,6 +56,12 @@ public class StandardCalc implements Calculator {
     }
   }
 
+  /**
+   * Returns a solution for infix notation expressions.
+   *
+   * @param expression The {@code String} to be evaluated
+   * @return The solution for the expression
+   */
   @Override
   public float evaluate(String expression) {
     // Split and store components of expression in an array
@@ -68,11 +79,13 @@ public class StandardCalc implements Calculator {
       } else if (part.equals("(")) {
         push(part);
       } else if (part.equals(")")) {
+        // If there is a close bracket, then keep popping operators until open bracket is found.
         while (!opStack.isEmpty() && !(opStack.peek().equals("("))) {
           postfix += opStack.pop().toString() + " ";
         }
         opStack.pop();
       } else {
+        //Keep popping if top operator on stack has a greater priority.
         while (!opStack.isEmpty() && (precedence(part) <= precedence(opStack.peek()))) {
           postfix += opStack.pop().toString() + " ";
         }
@@ -80,10 +93,12 @@ public class StandardCalc implements Calculator {
       }
     }
 
+    //pop any left over operators.
     while (!opStack.isEmpty()) {
       postfix += opStack.pop().toString() + " ";
     }
 
+    //postfix string has been created, so now just needs to be evaluated.
     return rpCalc.evaluate(postfix);
   }
 }
