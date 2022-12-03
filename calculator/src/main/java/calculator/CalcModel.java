4baf176cbf3e7e4c3ed0bcdb2595660bce7a41da
Hijithan <zjac082@live.rhul.ac.uk>
2022-12-03 14:39:21 +0000

    Add CalcModel state class
    Commit Description:
    Before connecting the standard clac assembly code to the GUI, i will
    first add a state class called CalcModel, which will switch between
    postfix and infix. To avoid the need of if statements.
    
    TDD:
    I first write a test case to initiate a class called calc model.
    The test fails and to fix this i just, create a class called ClacModel.
    
    Fearure Branch Description:
    This feature will combine infix and postfix to the GUI, so that the user
    will be able to select what type of expression they would like to
    evaluate.

---
+++ calculator/src/main/java/calculator/CalcModel.java
@@ -0,0 +1,10 @@
+package calculator;
+
+/**
+ * A state class which switches between postfix and infix.
+ *
+ * @author HIJI
+ */
+public class CalcModel {
+
+}
---
+++ calculator/src/test/java/calculator/TestCalcModel.java
@@ -0,0 +1,13 @@
+package calculator;
+
+import org.junit.jupiter.api.BeforeEach;
+
+class TestCalcModel {
+  private CalcModel calc;
+  
+  @BeforeEach
+  void setUp() {
+    calc = new CalcModel();
+  }
+
+}
