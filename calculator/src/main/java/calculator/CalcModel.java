bcbd07810996dd606280566625f9e361d74514af
Hijithan <zjac082@live.rhul.ac.uk>
2022-12-03 15:46:29 +0000

    CalcModel can do Standard evaluation
    Commit Description/TDD:
    I mae a test to see if CalcModel can change state to StandardCalc.
    The test fails because CalcModel does not have a setState method.
    To fix this i make a setState method which, chnages the current
    calculator state to Standard mode.

--- calculator/src/main/java/calculator/CalcModel.java
+++ calculator/src/main/java/calculator/CalcModel.java
@@ -6,5 +6,28 @@ package calculator;
  * @author HIJI
  */
 public class CalcModel {
-
+  private Calculator currentState;
+  private Calculator scalc;
+  
+  public CalcModel() {
+    scalc = new StandardCalc();
+  }
+  
+  /**
+   * Change calculator to either infix or postfix.
+   *
+   * @param isInfix Check if it should be in infix or not
+   */
+  public void setState(boolean isInfix) {
+    if (isInfix) {
+      currentState = scalc;
+    }
+  }
+  
+  /**
+   * Evaluates user expression either in infix or postfix.
+   */
+  public float evaluate(String expression) {
+    return currentState.evaluate(expression);
+  }
 }
--- calculator/src/test/java/calculator/TestCalcModel.java
+++ calculator/src/test/java/calculator/TestCalcModel.java
@@ -1,13 +1,23 @@
 package calculator;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 class TestCalcModel {
   private CalcModel calc;
+  private RevPolishCalc rcalc;
   
   @BeforeEach
   void setUp() {
     calc = new CalcModel();
+  }
+  
+  @Test
+  void testStandardEvaluate() {
+    calc.setState(true);
+    assertEquals(6.0f, calc.evaluate("3 + 3"));
   }
 
 }
