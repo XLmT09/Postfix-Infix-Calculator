a9d7b1a4fd7ed08e9f6f6b44bb713539fb0f22aa
Hijithan <zjac082@live.rhul.ac.uk>
2022-12-05 20:48:22 +0000

    [Refactor+Test]Test Factory, delete Entry
    Commit Description:
    I now modify testing in TestEntry which uses TDD testing to work with
    FactoryEntry instead of normal Entry.
    After i make test pass with FactoryEntry, i delete the original Entry
    class as it wont be needed anymore.

--- calculator/src/test/java/calculator/TestEntry.java
+++ calculator/src/test/java/calculator/TestEntry.java
@@ -10,14 +10,15 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class TestEntry {
-  private Entry numEntry;
-  private Entry symbolEntry;
-  private Entry strEntry;
+  private EntryInterface numEntry;
+  private EntryInterface symbolEntry;
+  private FactoryEntry fact;
   
   @BeforeEach
   void setup() {
-    numEntry = new Entry(2.0f);
-    symbolEntry = new Entry(Symbol.LEFT_BRACKET);
+    fact = new FactoryEntry();
+    numEntry = fact.createEntry(2.0f);
+    symbolEntry = fact.createEntry(Symbol.LEFT_BRACKET);
   }
   
   @Test
@@ -29,12 +30,6 @@ class TestEntry {
     assertEquals("+", Symbol.PLUS.toString());
     assertEquals("-", Symbol.MINUS.toString());
     assertEquals("invalid", Symbol.INVALID.toString());
-  }
-  
-  @Test
-  void testGetTypeString() {
-    assertEquals(Type.STRING, strEntry.getType(),
-        "A String Entry instance should have STRING type.");
   }
   
   @Test
@@ -68,12 +63,6 @@ class TestEntry {
     assertThrows(BadTypeException.class, () -> numEntry.getSymbol(), 
         "You cannot use getSymbol method if entry type NUMBER.");
   }
-  
-  @Test 
-  void testInvalidGetSymbolWithStrType() {
-    assertThrows(BadTypeException.class, () -> strEntry.getSymbol(), 
-        "You cannot use getSymbol method if entry type is SYMBOL.");
-  }
 
   @Test
   void testGetValue() {
@@ -88,19 +77,11 @@ class TestEntry {
   }
   
   @Test 
-  void testInvalidGetValueWithStrType() {
-    assertThrows(BadTypeException.class, () -> strEntry.getValue(), 
-        "You cannot use getValue method if entry type is STRING.");
-  }
-  
-  @Test 
   void tesEntriesOfDifferentTypesArePrintable() {
     assertEquals("2.0", numEntry.toString(), 
         "toString returns float in string format for Entry type NUMBER.");
     assertEquals("(", symbolEntry.toString(), 
         "toString retruns corrosponding enum code for Entry type SYMBOL.");
-    assertEquals("2", strEntry.toString(), 
-        "toString method returns str variable for type Entry STRING.");
   }
   
 
@@ -108,34 +89,28 @@ class TestEntry {
   void testEntriesAreEqual() {
     assertEquals(numEntry.equals(numEntry), true, 
         "Check if Entry is equal to itself.");
-    assertEquals(numEntry.equals(new Entry(2.0f)), true, 
+    assertEquals(numEntry.equals(fact.createEntry(2.0f)), true, 
         "If Entry is NUMBER type and has the same number value then returns true.");
-    assertEquals(symbolEntry.equals(new Entry(Symbol.LEFT_BRACKET)), true, 
+    assertEquals(symbolEntry.equals(fact.createEntry(Symbol.LEFT_BRACKET)), true, 
         "If Entry has same SYMBOL type and the same Symbol enum then return true.");
   }
 
   @Test 
   void testEntriesAreNotEqual() {
-    assertEquals(numEntry.equals(new Entry(6.0f)), false,
+    assertEquals(numEntry.equals(fact.createEntry(6.0f)), false,
         "return false as Entry float values are different values.");
-    assertEquals(symbolEntry.equals(new Entry(Symbol.RIGHT_BRACKET)), false,
+    assertEquals(symbolEntry.equals(fact.createEntry(Symbol.RIGHT_BRACKET)), false,
         "return false as symbols are different enums.");
     assertEquals(numEntry.equals(symbolEntry), false, 
         "return false as Entry type NUMBER and SYMBOL are different.");
-    assertEquals(strEntry.equals(numEntry), false, 
-        "return false as Entry type STRING and NUMBER are different.");
-    assertEquals(strEntry.equals(symbolEntry), false, 
-        "return false as Entry type STRING and SYMBOL are different.");
   }
   
   @Test 
   void testEntryHashCode() {
     assertEquals(numEntry.hashCode() == numEntry.hashCode(), true, 
             "return true as they are the same insatnce.");
-    assertEquals(numEntry.hashCode() == new Entry(2.0f).hashCode(), true, 
+    assertEquals(numEntry.hashCode() == fact.createEntry(2.0f).hashCode(), true, 
             "return true as they are same class of same values.");
-    assertEquals(strEntry.equals(numEntry), false, 
-            "return false as Entry types are different.");
   }
   
 }
