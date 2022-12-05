705bc3e58640a696f97d8f4f27a1a6c412266175
Hijithan <zjac082@live.rhul.ac.uk>
2022-12-05 20:26:57 +0000

    [Refactoring] Create two Entry class types
    Commit Description:
     I create two new Entry types one called NumEntry and another called
     SymbolEntry, where they create entries representing there types. I
     essentially just copy from the Entry class and also make sure the class
     implments EntryInterface.

---
+++ calculator/src/main/java/calculator/SymbolEntry.java
@@ -0,0 +1,77 @@
+package calculator;
+
+import enums.Symbol;
+import enums.Type;
+import exceptions.BadTypeException;
+import java.util.Objects;
+
+/**
+ * An Entry which contains only type symbols.
+ *
+ * @author HIJI
+ */
+public class SymbolEntry implements EntryInterface {
+  private Symbol sym;
+  private Type type;
+
+  /**
+   * Constructs a new {@code Entry} of type {@code NUMBER}.
+   *
+   * @param val The symbol the {@code Entry} class will hold
+   */
+  public SymbolEntry(Symbol val) {
+    sym = val;
+    type = Type.SYMBOL;
+  }
+
+  /**
+   * Returns the {@code Type} of an {@code Entry}.
+   */
+  public Type getType() {
+    return type;
+  }
+
+  /**
+   * Returns the {@code Symbol} value an {@code Entry} represents.
+   */
+  public Symbol getSymbol() {
+    return sym;
+  }
+
+  /**
+   * Returns the float value an {@code Entry} represents.
+   *
+   * @throws BadTypeException If {@code Entry} not of {@code Type NUMBER}
+   */
+  public float getValue() {
+    throw new BadTypeException("Entry must be Type: NUMBER");
+  }
+  
+  @Override
+  public int hashCode() {
+    return Objects.hash(sym, type);
+  }
+
+  /**
+   * Returns true if two entries have the same {@code Type} and number.
+   */
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj) {
+      return true;
+    }
+    if (obj == null) {
+      return false;
+    }
+    if (getClass() != obj.getClass()) {
+      return false;
+    }
+    EntryInterface otherEntry = (EntryInterface) obj;
+    return type.equals(otherEntry.getType()) && sym == otherEntry.getSymbol();
+  }
+  
+  @Override
+  public String toString() {
+    return "" + sym;
+  }
+}
